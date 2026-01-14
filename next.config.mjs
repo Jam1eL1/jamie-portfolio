c505ffb0ebf22a611108c6f8a94d0fc4fb9619a3
Jam1eL1 <23jisu@gmail.com>
2026-01-13 18:37:08 -0800

    chore: enable SVGR for SVG imports using Turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -2,52 +2,13 @@
 const nextConfig = {
   reactCompiler: true,
 
-  webpack(config) {
-    // Find the existing rule that handles images/assets and make sure it DOESN'T grab svg
-    const assetRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
-    if (assetRule) {
-      assetRule.exclude = /\.svg$/i;
-    }
-
-    // Add a dedicated SVG rule:
-    // - `import iconUrl from "./icon.svg?url"` -> gives URL string (asset)
-    // - `import Icon from "./icon.svg"` -> gives React component (SVGR)
-    config.module.rules.push({
-      test: /\.svg$/i,
-      oneOf: [
-        {
-          resourceQuery: /url/, // *.svg?url
-          type: "asset/resource",
-        },
-        {
-          // default: treat as React component
-          use: [
-            {
-              loader: "@svgr/webpack",
-              options: {
-                exportType: "default",
-                svgo: true,
-                svgoConfig: {
-                  plugins: [
-                    {
-                      name: "preset-default",
-                      params: {
-                        overrides: {
-                          // keep viewBox so sizing works
-                          removeViewBox: false,
-                        },
-                      },
-                    },
-                  ],
-                },
-              },
-            },
-          ],
-        },
-      ],
-    });
-
-    return config;
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
 };
 
